5b2d0fc8a19cfc1b33b8cc6debcc2023e00df7a3
Transcendence -- <77237927+Ruizheee@users.noreply.github.com>
2024-07-22 19:18:18 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+	String url = "http://192.168.154.130"; // Change This
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
